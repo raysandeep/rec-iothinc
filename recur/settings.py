65709210eddca154c744f294c53fe8ecc5aed5cb
Sai Sandeep Rayanuthala <43823311+unknown-guy-1610@users.noreply.github.com>
2019-12-06 02:28:16 +0530

    Update settings.py

--- recur/settings.py
+++ recur/settings.py
@@ -51,7 +51,8 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
